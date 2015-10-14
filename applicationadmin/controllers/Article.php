c4c791f3ca32053839e414296795f14131df0da0
long365 <809692975@qq.com>
2015-10-14 18:27:18 +0800

    增加文章

---
+++ applicationadmin/controllers/Article.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Article extends CI_Controller {
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Article_model');
+		$this->isLogin();
+	}
+	
+	/**
+	 * 检测是否登陆
+	 */
+	function isLogin()
+	{
+	    if(!($this->session->userId) || ($this->session->userId) <= 0)
+	    {
+	        header("Location:/Index/login");
+	    }
+	    return true;
+	}
+	
+	/**
+	 * 发表文章
+	 */
+	function addArticle()
+	{
+		$this->load->view('addArticle');
+	}
+	
+	/**
+	 * 处理文章入库
+	 */
+	function addArticleDeal()
+	{
+		// 标题
+		$article_title = $this->input->post_get('article_title');
+		// 内容
+		$content = $this->input->post_get('content');
+		if(!$article_title || !$content)
+		{
+			$this->load->view('failure');
+			exit;
+		}
+		$article_title = addslashes($article_title); 
+		$content = addslashes($content);
+		$article_flag = intval($this->input->post_get('article_flag'));
+		$article_headline = addslashes($this->input->post_get('article_headline'));
+		$article_keywords = addslashes($this->input->post_get('article_keywords'));
+		$article_description = addslashes($this->input->post_get('article_description'));
+		$article_click = 1;
+		$article_datetime = time();
+		$res = $this->Article_model->addArticleDeal($article_title,$article_flag,$article_datetime,$article_headline,$article_keywords,$article_description,$article_click,$content);
+		if($res)
+		{
+			$this->load->view('success');
+		}else {
+			$this->load->view('failure');
+		}
+	}
+
+}
